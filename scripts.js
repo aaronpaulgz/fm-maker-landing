c1ddbef2adfd0171bbf3f7c4af06e57c89b0aead
Aaron Gonzalez <aarong.1503@gmail.com>
2021-01-24 10:41:37 -0500

    Tablet mode ready

--- scripts.js
+++ scripts.js
@@ -9,5 +9,21 @@ const validateMob = () => {
     $mobBtn.style.margin = "8px 0 0 0";
     $mobInput.style.border = "solid 2px var(--red)";
   } else {
+    location.reload();
+  }
+};
+
+let $Input = document.getElementById("input");
+let $Btn = document.getElementById("button");
+let $Warn = document.getElementById("warn");
+let $Footer = document.getElementById("footer");
+
+const validate = () => {
+  let $Input = document.getElementById("input");
+  if ($Input.value <= 6) {
+    $Warn.style.visibility = "visible";
+    $Input.style.border = "solid 2px var(--red)";
+  } else {
+    location.reload();
   }
 };
